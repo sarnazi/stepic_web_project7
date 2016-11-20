19456bf35c2481e6ad367057a97a3852ac7143a3
sarnazi <sarnazi@mail.ru>
2016-11-20 20:15:47 +0500

    urok1

--- etc/init.sh
+++ etc/init.sh
@@ -1,4 +1,6 @@
+sudo rm -rf /etc/nginx/sites-enabled/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
